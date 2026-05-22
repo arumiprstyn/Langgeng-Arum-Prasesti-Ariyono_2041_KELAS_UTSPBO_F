de5f3723e0e13474373a22e0e81ca4efd1a50248
arumiprstyn <langgengarum41@gmail.com>
2026-05-22 09:20:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UTS_PBO;
+
+namespace UTS_PBO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BukuReferensi buku2 = new BukuReferensi("Rumi", "A102", "To Kill a Mockingbird");
+
+            buku2.TampilkanInfo();
+            Console.WriteLine("Total Biaya Pinjam: " + buku2.HitungBiayaPinjam() + " rupiah");
+        }
+    }
+}
---
+++ class1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace UTS_PBO
+{
+    public abstract class PinjamBuku
+    {
+        private string namaAnggota;
+        private string idAnggota;
+        private string judulBuku;
+
+        public PinjamBuku(string namaAnggota, string idAnggota, string judulBuku)
+        {
+            this.namaAnggota = namaAnggota;
+            this.idAnggota = idAnggota;
+            this.judulBuku = judulBuku;
+        }
+
+        public string GetNamaAnggota()
+        {
+            return namaAnggota;
+        }
+
+        public string GetIdAnggota()
+        {
+            return idAnggota;
+        }
+
+        public string GetJudulBuku()
+        {
+            return judulBuku;
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Nama Anggota: " + namaAnggota);
+            Console.WriteLine("ID Anggota: " + idAnggota);
+            Console.WriteLine("Judul Buku: " + judulBuku);
+        }
+        public abstract double HitungBiayaPinjam();
+    }
+    class BukuReguler : PinjamBuku
+    {
+        public int biayaPerHari = 1000;
+        public int lamaHari;
+        public BukuReguler(string namaAnggota, string idAnggota, string judulBuku)
+            : base(namaAnggota, idAnggota, judulBuku)
+        {
+            this.biayaPerHari = biayaPerHari;
+            this.lamaHari = lamaHari;
+        }
+        public override double HitungBiayaPinjam()
+        {
+            return biayaPerHari * lamaHari;
+        }
+    }
+    internal class BukuReferensi : PinjamBuku
+    {
+        public int biayaPerHari = 1000;
+        public int biayaAsuransi = 2000;
+        public int lamaHari = 5;
+        public BukuReferensi(string namaAnggota, string idAnggota, string judulBuku)
+            : base(namaAnggota, idAnggota, judulBuku)
+        {
+            this.biayaPerHari = biayaPerHari;
+            this.biayaAsuransi = biayaAsuransi;
+        }
+        public override double HitungBiayaPinjam()
+        {
+            return (biayaPerHari * lamaHari) + biayaAsuransi;
+        }
+    }
+}
